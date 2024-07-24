d6e0231127e38f557142289fd650d024b396b734
Vinoth <vd2@sanger.ac.uk>
2024-07-24 13:57:58 +0100

    Upload dummy credentials

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import os
 os.environ["aws_access_key_id"]='DUMMY'
 os.environ["aws_secret_access_key"]='DUMMY'
 aws_secret_access_key = "YHIUYUYOSEYGTGE"
+aws_secret_access_key = "SJHGDJKGUWEUGGDJS"
 print(os.environ["aws_access_key_id"])
