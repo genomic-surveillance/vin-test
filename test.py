508734ed95546a4424afe20b9204cff6e0f6618c
Vinoth <vd2@sanger.ac.uk>
2024-07-25 11:15:23 +0100

    Test secret

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["aws_access_key_id"]='DUMMY'
-os.environ["aws_secret_access_key"]='DUMMY'
-aws_secret_access_key = "YHIUYUYOSEYGTGE"
-aws_secret_access_key = "SJHGDJKGUWEUGGDJS"
-password='Solarisete'
-print(os.environ["aws_access_key_id"])
